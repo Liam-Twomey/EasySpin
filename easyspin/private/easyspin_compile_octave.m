5f59055bf81fd414c03266371d098eb82798c7f7
Liam Twomey <lptwomey@ucdavis.edu>
2026-07-16 14:33:43 -0700

    Squash bugs in mkoctfile compilation of c binaries

--- easyspin/private/easyspin_compile_octave.m
+++ easyspin/private/easyspin_compile_octave.m
@@ -1,9 +1,14 @@
-function easyspin_compile()
+function easyspin_compile_octave()
 
 % Determine directory containing mex source files
 %-------------------------------------------------------------------------------
-esPath = fileparts(which(mfilename));
-esPrivateFolder = esPath;
+%a = mfilename("fullpath")
+%fprintf('0: %s\n',a)
+esPrivateFolder = fileparts(mfilename("fullpath"));
+%fprintf(esPrivateFolder)
+%fprintf('1: %s\n',which(a))
+%fprintf('1: %s\n',fileparts(a))
+%esPrivateFolder = esPath;
 octDirectory = esPrivateFolder;
 disp(['  directory: ' octDirectory]);
 disp(['  version: ' version]);
@@ -11,40 +16,40 @@ disp(['  version: ' version]);
 % Change directory
 olddir = pwd;
 cd(octDirectory);
+fprintf('in dir: %s',pwd)
 
 % Determine mex configuration
 %-------------------------------------------------------------------------------
-fprintf("The C compiler available to mkoctfile is:")
-cc = mkoctfile -p CC
+cc = mkoctfile('-p','CC');
 if numel(cc)==0
-  error('MEX is not configured for C. Run mex -setup C.');
+  error('No C compiler is available. Please install one or make sure it is available on $PATH');
 else
-  fprintf('  oct C compiler: %s\n',cc(1).Name); 
+  fprintf('  oct C compiler: %s\n',cc); 
 end
-
 
 % Get list of *.c files
 %-------------------------------------------------------------------------------
 SourceFiles = dir('*.c');
 nFiles = numel(SourceFiles);
 
-
 % Compile and link mex files
 %-------------------------------------------------------------------------------
-fprintf('  compiling %d c-files...\n',nFiles);
-fprintf('  compiling %d c-files...\n',nFiles);
+if nFiles==0
+	error('No C files found to compile.')
+else 
+	fprintf('  compiling %d c-files...\n',nFiles);
+end
+
 for f = 1:nFiles
   fprintf('  (%d/%d) %-25s ',f,nFiles,SourceFiles(f).name);
   try
-	mkoctfile SourceFiles(f).name;
+	mkoctfile(char(SourceFiles(f).name));
     fprintf('  complete\n');
     ok(f) = true;
   catch
     fprintf('  failed\n');
     disp(lasterr);
-	disp("Please make sure that a C compiler such as gcc and the development headers ...
-		for Octave are installed. Depending on you system, this may be called `octave-devel` ...
-		or `octave-dev`. Please consult your package manager.")
+	disp("Please make sure that a C compiler such as gcc and the development headers for Octave are installed. Depending on you system, this may be called `octave-devel` or `octave-dev`. Please consult your package manager.")
     ok(f) = false;  %#ok
   end
 end
